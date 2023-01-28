4d58bae02766f1db900d0a4d5aeb33ca29f173e7
emmajane1313 <admin@digitalax.xyz>
2023-01-28 05:58:49 -0500

    init

--- next.config.js
+++ next.config.js
@@ -1,7 +1,33 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "thedial.infura-ipfs.io",
+        pathname: "/ipfs/**",
+      },
+      {
+        protocol: "https",
+        hostname: "media.tenor.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "lexica-serve-encoded-images.sharif.workers.dev/md",
+        pathname: "/**",
+      },
+    ],
+    unoptimized: true,
+  },
+  experimental: {
+    scrollRestoration: true,
+    appDir: false,
+    esmExternals: true,
+  },
+};
 
 module.exports = nextConfig
